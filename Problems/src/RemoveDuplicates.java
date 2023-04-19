5678d57a1745573e0736b6030512193a4f20c61c
Prakash Khatua <prakashkhatua100@gmail.com>
2023-04-19 20:14:38 +0530

    Update problems

--- Problems/src/RemoveDuplicates.java
+++ Problems/src/RemoveDuplicates.java
@@ -3,10 +3,7 @@ import java.util.HashSet;
 public class RemoveDuplicates {
     public static void main(String[] args) {
          int[] arr ={4,5,4,5,5,5,9};
-         int k=Duplicates(arr);
-         for(int i=0;i<k;i++){
-             System.out.println(arr[i]+" ");
-         }
+         Duplicates(arr);
     }
 
     public static int Duplicates(int[] nums){
@@ -19,6 +16,9 @@ public class RemoveDuplicates {
         int j=0;
         for(int set: temp){
             nums[j++]=set;
+        }
+        for(int i=0;i< temp.size();i++){
+            System.out.println(nums[i]);
         }
         return temp.size();
 
