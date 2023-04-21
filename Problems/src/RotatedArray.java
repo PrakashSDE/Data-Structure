679f3321b0011ccd81a2f5eb8b8fd9f8dc07d1bf
Prakash Khatua <prakashkhatua100@gmail.com>
2023-04-21 22:41:43 +0530

    Update

--- Problems/src/RotatedArray.java
+++ Problems/src/RotatedArray.java
@@ -1,11 +1,31 @@
 public class RotatedArray {
     public static void main(String[] args) {
-
+        int[] arr ={1,2,7,9,10,14};
+        rightRotated(arr,4);
     }
 
     public static void rightRotated(int[] nums, int k){
-        for(int i=0; i<=k;i++){
-
+        int[] nums2 = new int[nums.length];
+        int j=0;
+        for(int i= (nums.length - k);i<nums.length;i++){
+            nums2[j]=nums[i];
+            j++;
         }
+
+
+
+        for(int i=0; k<nums.length; i++){
+            nums2[k]=nums[i];
+            k++;
+        }
+
+        System.arraycopy(nums2, 0, nums, 0, nums.length);
+
+        for(int a: nums){
+            System.out.println(a);
+        }
+
+
+
     }
 }
